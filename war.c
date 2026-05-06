0e05fd8de63a0a409ca959dff9bec47fb99094a5
WashingtonCode <202501483143@alunos.unifanor.edu.br>
2026-05-06 02:44:43 +0000

    Cadastro utilizando ponteiros

--- war.c
+++ war.c
@@ -79,3 +79,21 @@ int main() {
     printf("\nPrograma encerrado. Memoria liberada.\n");
     return 0;
 }
+
+// Cadastro utilizando aritmética de ponteiros
+void cadastrarTerritorios(Territorio* mapa, int quantidade) {
+    for (int i = 0; i < quantidade; i++) {
+        printf("\nTerritorio %d:\n", i + 1);
+        printf("Nome do territorio: ");
+        fgets((mapa + i)->nome, 30, stdin);
+        (mapa + i)->nome[strcspn((mapa + i)->nome, "\n")] = 0; // Remove o \n
+
+        printf("Cor do Exercito: ");
+        fgets((mapa + i)->corExercito, 10, stdin);
+        (mapa + i)->corExercito[strcspn((mapa + i)->corExercito, "\n")] = 0;
+
+        printf("Quantidade de Tropas: ");
+        scanf("%d", &(mapa + i)->qtdTropas);
+        getchar(); // Limpa buffer
+    }
+}
