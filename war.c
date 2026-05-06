628c9ae122bb60ad4069d2d4613d060a08d73e63
WashingtonCode <202501483143@alunos.unifanor.edu.br>
2026-05-06 02:39:28 +0000

    Definição da struct para representar um território

--- war.c
+++ war.c
@@ -8,3 +8,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+
+// Definição da struct para representar um território
+typedef struct {
+    char nome[30];
+    char corExercito[10];
+    int qtdTropas;
+} Territorio;
